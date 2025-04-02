e1b5d57196b30c6db31a5a0347d7bfff37d34fee
Joaquin <joaquin.telleria@wh-d.at>
2025-04-02 14:46:36 +0200

    Removed default tailwind themeing in favor of mui themeing, added tailwind test in buttons

--- src/app/about/about.tsx
+++ src/app/about/about.tsx
@@ -5,7 +5,10 @@ export default function About() {
     <div className="min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <h1 className="text-2xl font-bold mb-4">About Us</h1>
       <p className="text-lg">Welcome to our website. This is the about page.</p>
-      <Button>Hello World!</Button>
+      <div className="flex flex-col gap-4 w-fit">
+        <Button>Hello World! 1</Button>
+        <Button>Hello World! 2</Button>
+      </div>
     </div>
   );
 }
--- src/app/home.tsx
+++ src/app/home.tsx
@@ -4,7 +4,10 @@ export function Home() {
   return (
     <div className="min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <h1 className="text-2xl font-bold mb-4">Next JS MUI Quickstack</h1>
-      <Button>Hello World!</Button>
+      <div className="flex flex-col gap-4 w-fit">
+        <Button>Hello World! 1</Button>
+        <Button>Hello World! 2</Button>
+      </div>
     </div>
   );
 }
